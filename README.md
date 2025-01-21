2827577a3e9d1e585c203598a0db8e34c63c0a43
Fabio Mierlo <fmierlo@users.noreply.github.com>
2025-01-21 16:13:44 -0300

    Added a simple example

--- README.md
+++ README.md
@@ -4,7 +4,72 @@ Mockdown is a single file and macro/dependency free mock library for Rust.
 
 # Example
 
-## mod sys
+## Simple
+
+```rust
+#[cfg(not(test))]
+mod math {
+   pub fn add(x: i32, y: i32) -> i32 {
+       x + y
+   }
+}
+
+mod lib {
+   #[cfg(not(test))]
+   use super::math;
+
+   #[cfg(test)]
+   use mocks::math;
+
+   fn add(x: i32, y: i32) -> i32 {
+       math::add(x, y)
+   }
+
+   #[cfg(test)]
+   mod mocks {
+       pub mod math {
+           use mockdown::{Mockdown, Static};
+           use std::{cell::RefCell, thread::LocalKey};
+
+           thread_local! {
+               static MOCKDOWN: RefCell<Mockdown> = Mockdown::thread_local();
+           }
+
+           pub fn mockdown() -> &'static LocalKey<RefCell<Mockdown>> {
+               &MOCKDOWN
+           }
+
+           #[derive(Debug, PartialEq)]
+           pub struct Add(pub i32, pub i32);
+
+           pub fn add(x: i32, y: i32) -> i32 {
+               let args = Add(x, y);
+               MOCKDOWN.mock(args).unwrap()
+           }
+       }
+   }
+
+   #[cfg(test)]
+   mod tests {
+       use super::math;
+       use mockdown::Static;
+
+       #[test]
+       fn test_add() {
+           math::mockdown().expect(|args| {
+               assert_eq!(math::Add(1, 1), args);
+               2
+           });
+
+           let z = super::add(1, 1);
+           assert_eq!(z, 2);
+       }
+   }
+}
+```
+## Libc
+
+### mod sys
 
 ```rust
 use libc::{c_int, c_ulong, c_void};
@@ -28,7 +93,8 @@ pub fn errno() -> c_int {
 }
 ```
 
-## mod socket
+### mod socket
+
 ```rust
 #[cfg(not(test))]
 use super::sys;
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,72 @@
+//!
+//!Mockdown is a single file and macro/dependency free mock library for Rust.
+//!
+//!# Examples
+//!
+//!## Simple
+//!
+//!```
+//!#[cfg(not(test))]
+//!mod math {
+//!    pub fn add(x: i32, y: i32) -> i32 {
+//!        x + y
+//!    }
+//!}
+//!
+//!mod lib {
+//!    #[cfg(not(test))]
+//!    use super::math;
+//!
+//!    #[cfg(test)]
+//!    use mocks::math;
+//!
+//!    fn add(x: i32, y: i32) -> i32 {
+//!        math::add(x, y)
+//!    }
+//!
+//!    #[cfg(test)]
+//!    mod mocks {
+//!        pub mod math {
+//!            use mockdown::{Mockdown, Static};
+//!            use std::{cell::RefCell, thread::LocalKey};
+//!
+//!            thread_local! {
+//!                static MOCKDOWN: RefCell<Mockdown> = Mockdown::thread_local();
+//!            }
+//!
+//!            pub fn mockdown() -> &'static LocalKey<RefCell<Mockdown>> {
+//!                &MOCKDOWN
+//!            }
+//!
+//!            #[derive(Debug, PartialEq)]
+//!            pub struct Add(pub i32, pub i32);
+//!
+//!            pub fn add(x: i32, y: i32) -> i32 {
+//!                let args = Add(x, y);
+//!                MOCKDOWN.mock(args).unwrap()
+//!            }
+//!        }
+//!    }
+//!
+//!    #[cfg(test)]
+//!    mod tests {
+//!        use super::math;
+//!        use mockdown::Static;
+//!
+//!        #[test]
+//!        fn test_add() {
+//!            math::mockdown().expect(|args| {
+//!                assert_eq!(math::Add(1, 1), args);
+//!                2
+//!            });
+//!
+//!            let z = super::add(1, 1);
+//!            assert_eq!(z, 2);
+//!        }
+//!    }
+//!}
+//! ```
+
 mod mockdown;
 
 pub use mockdown::*;
