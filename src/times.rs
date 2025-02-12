a080dc504f872bd8fb7f9cabf0ed427806131a1f
Fabio Mierlo <fmierlo@users.noreply.github.com>
2025-02-12 19:01:55 -0300

    Implement times method for concurrent types: RefCell, ThreadLocal, and Global

--- src/times.rs
+++ src/times.rs
@@ -1,12 +1,21 @@
 use std::any::Any;
+use std::cell::RefCell;
+use std::sync::{Arc, LazyLock, Mutex};
+use std::thread::LocalKey;
 
-use crate::Mock;
+use crate::{Mock, Mockdown};
 
 pub trait MockTimes: Mock {
-    fn times<T: Any, U: Any>(&'static self, times: u8, expect: fn(T) -> U) -> &'static Self {
+    fn times<E: Any + Send + Clone>(&'static self, times: u8, expect: E) -> &'static Self {
         for _ in 0..times {
-            self.expect(expect);
+            self.expect(expect.clone());
         }
         self
     }
 }
+
+impl MockTimes for RefCell<Mockdown> {}
+
+impl MockTimes for LocalKey<RefCell<Mockdown>> {}
+
+impl MockTimes for LazyLock<Arc<Mutex<Mockdown>>> {}
